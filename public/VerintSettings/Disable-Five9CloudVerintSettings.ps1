9fa4118d4128e9888ededaba0aa3562790ac4e62
Justin Gray <jgray82@protonmail.com>
2025-10-27 16:49:34 -0400

    Created disable function to shut off verint packages flag completely. Rewrote Remove to allow for individual removals. Added username helper. Deleted deprecated update function

---
+++ public/VerintSettings/Disable-Five9CloudVerintSettings.ps1
@@ -0,0 +1,20 @@
+﻿function Disable-Five9CloudVerintSettings {
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory = $true)][string]$UserUID
+    )
+    
+    if (-not (Test-Five9CloudConnection)) { return }
+    
+    $uri = "$($global:Five9CloudToken.ApiBaseUrl)/wfo-verint-config/v1/domains/$($global:Five9CloudToken.DomainId)/users/$UserUID/verint-settings"
+    
+    try {
+        Invoke-RestMethod -Uri $uri -Method Delete -Headers @{
+            Authorization = "Bearer $($global:Five9CloudToken.AccessToken)"
+            'Content-Type' = 'application/json'
+        }
+    } catch {
+        Write-Error "Failed to remove Verint settings: $_"
+    }
+}
--- public/VerintSettings/Remove-Five9CloudVerintSettings.ps1
+++ public/VerintSettings/Remove-Five9CloudVerintSettings.ps1
@@ -1,22 +1,67 @@
-﻿# Five9Cloud PowerShell Module
-# Function: Remove-Five9CloudVerintSettings
-# Category: VerintSettings
-function Remove-Five9CloudVerintSettings {
+﻿function Remove-Five9CloudVerintSettings {
     [CmdletBinding()]
     param (
-
-        [Parameter(Mandatory = $true)][string]$UserUID
+        [Parameter(Mandatory = $true, ParameterSetName = 'ById')]
+        [string]$UserUID,
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByName')]
+        [string]$userName,
+        [Parameter(Mandatory = $true)]
+        [ValidateSet(
+            'call-recording',
+            'call-recording.quality-monitoring',
+            'call-recording.quality-monitoring.analytics-driven-quality',
+            'call-recording.speech-analytics',
+            'workforce-management',
+            'performance-management',
+            'call-recording.advanced-desktop-analytics'
+        )]
+        [string[]]$Packages
     )
     
     if (-not (Test-Five9CloudConnection)) { return }
+
+    # If userName is provided, resolve it to userUID
+    if ($PSCmdlet.ParameterSetName -eq 'ByName') {
+        Write-Verbose "Resolving user name '$userName' to userUID..."
+        
+        try {
+            $userLookup = Get-Five9CloudUser -Filter "username=='$($userName)'" -Fields "id,userUID"
+            if ($userLookup.resultsCount -gt 0) {
+                $UserUID = $userLookup.entities[0].userUID
+                Write-Verbose "Found user with ID: $userName"
+            }
+        } catch {
+            Write-Verbose "No user found with name '$userName'"
+        }
+        
+        # If not found, throw error
+        if (-not $UserUID) {
+            Write-Error "User with name '$userName' not found."
+            return
+        }
+    }
     
     $uri = "$($global:Five9CloudToken.ApiBaseUrl)/wfo-verint-config/v1/domains/$($global:Five9CloudToken.DomainId)/users/$UserUID/verint-settings"
+
+    # Get current packages
+    Write-Verbose "Resolving user name '$userName' to userUID..."
+        
+    try {
+        $packageLookup = Get-Five9CloudVerintSettings -UserUID "$($UserUID)"
+        $Packages = $packageLookup.packages | Where-Object { $_ -notin $Packages }
+    } catch {
+        Write-Verbose "Package not found with '$userUID'"
+    }
+
+    $body = @{
+        packages = $Packages
+    } | ConvertTo-Json
     
     try {
-        Invoke-RestMethod -Uri $uri -Method Delete -Headers @{
+        Invoke-RestMethod -Uri $uri -Method Put -Headers @{
             Authorization = "Bearer $($global:Five9CloudToken.AccessToken)"
             'Content-Type' = 'application/json'
-        }
+        } -Body $body
     } catch {
         Write-Error "Failed to remove Verint settings: $_"
     }
