6f852d703d7007f7ac046ba2ce3ee4eb138fdcb6
Justin Gray <justingray82@gmail.com>
2025-11-05 16:22:30 -0500

    adding Set-Five9CloudRecordingSettings to allow CRM Enable

---
+++ public/Campaigns/Recordings/Set-Five9CloudRecordingSettings.ps1
@@ -0,0 +1,26 @@
+﻿function Set-Five9CloudRecordingSettings {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string]$CampaignId,
+        [bool]$CRMEnabled
+    )
+    
+    if (-not (Test-Five9CloudConnection -AuthType CloudAuth)) { return }
+    
+    $uri = "$($global:Five9CloudToken.ApiBaseUrl)/recordings/v1/domains/$($global:Five9CloudToken.DomainId)/campaigns/$($CampaignId)/recording-settings"
+    
+    $headers = @{
+        Authorization = "Bearer $($global:Five9CloudToken.AccessToken)"
+        'Content-Type' = 'application/json'
+    }
+    
+    $body = @{}
+    
+    if ($CRMEnabled -ne $null) { $body['recordingSettingsEnabled'] = $CRMEnabled }
+    
+    try {
+        Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -Body ($body | ConvertTo-Json -Depth 10)
+    } catch {
+        Write-Error "Failed to update inbound campaign: $_"
+    }
+}
