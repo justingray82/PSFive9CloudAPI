860e2a77a1644302fc9e9025c934ad12c29a2d53
Justin Gray <justingray82@gmail.com>
2025-12-11 14:17:10 -0500

    Added Reason Code Sets

---
+++ public/Users/ReasonCodes/Get-Five9CloudReasonCodeSets.ps1
@@ -0,0 +1,32 @@
+﻿function Get-Five9CloudReasonCodeSets {
+    [CmdletBinding()]
+    param (
+        [string]$Filter
+    )
+    
+    if (-not (Test-Five9CloudConnection -AutoReconnect)) { return }
+    
+    # Original: Get-Five9CloudVerintSettings
+    $uri = "$($global:Five9CloudToken.ApiBaseUrl)/agent-sessions/v1/domains/$($global:Five9CloudToken.DomainId)/reason-code-sets"
+    
+    if ($Filter) {
+        $uri = "$($global:Five9CloudToken.ApiBaseUrl)/agent-sessions/v1/domains/$($global:Five9CloudToken.DomainId)/reason-code-sets?filter=$($Filter)"
+    } else {
+        $uri = "$($global:Five9CloudToken.ApiBaseUrl)/agent-sessions/v1/domains/$($global:Five9CloudToken.DomainId)/reason-code-sets"
+    }
+
+
+    try {
+        Invoke-RestMethod -Uri $uri -Method Get -Headers @{
+            Authorization = "Bearer $($global:Five9CloudToken.AccessToken)"
+            'Content-Type' = 'application/json'
+        }
+    } catch {
+        # Handle 404 specifically - this means no Verint settings exist for the user
+        if ($_.Exception.Response.StatusCode -eq 404) {
+            Write-Host "No Reason Code Sets found"
+        } else {
+            Write-Error "Failed to get Reason Code Sets: $_"
+        }
+    }
+}
---
+++ public/Users/ReasonCodes/Set-Five9CloudReasonCodeSet.ps1
@@ -0,0 +1,61 @@
+﻿function Set-Five9CloudReasonCodeSet {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ParameterSetName = 'ById')]
+        [string]$UserID,
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByName')]
+        [string]$UserName,
+        [Parameter()]
+        [string]$ReasonCodeSet
+    )
+    
+    if (-not (Test-Five9CloudConnection -AutoReconnect)) { return }
+
+    # If UserName is provided, resolve it to UserUID
+    if ($PSCmdlet.ParameterSetName -eq 'ByName') {
+        Write-Verbose "Resolving user name '$ByName' to UserUID..."
+        
+        try {
+            $UserLists = Get-Five9CloudUserList -Filter "username=='$($UserName)'"
+            if ($UserLists.items.count -gt 0) {
+                $UserID = $UserLists.items.userUID
+                Write-Verbose "Found user with ID: $UserName"
+            }
+        } catch {
+            Write-Verbose "No user found with name '$UserName'"
+        }
+        
+        # If still not found, throw error
+        if (-not $UserID) {
+            Write-Error "User with name '$UserName' not found."
+            return
+        }
+    }
+
+    Write-Verbose "Resolving reason code set '$ReasonCodeSet' to reasonCodeSetId..."
+        
+    try {
+        $SetList = Get-Five9CloudReasonCodeSets -Filter "name=='$($ReasonCodeSet)'"
+        if ($SetList.items.count -gt 0) {
+            $ReasonCodeSetID = $SetList.items.reasonCodeSetId
+        }
+    } catch {
+        Write-Verbose "No Reason Code Set found with name '$ReasonCodeSet'"
+    }
+            
+    # If still not found, throw error
+    if (-not $UserID) {
+        Write-Error "Reason code Set '$ReasonCodeSet' not found."
+        return
+    }
+    
+    $uri = "$($global:Five9CloudToken.ApiBaseUrl)/agent-sessions/v1/domains/$($global:Five9CloudToken.DomainId)/reason-code-sets/$($ReasonCodeSetID)/users/$($UserID)"
+    
+    try {
+        Invoke-RestMethod -Uri $uri -Method POST -Headers @{
+            Authorization = "Bearer $($global:Five9CloudToken.AccessToken)"
+        } | Out-Null
+    } catch {
+        Write-Error "Failed to add record set to users: $_"
+    }
+}
