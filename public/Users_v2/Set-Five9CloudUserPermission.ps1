22d474d282f8b816b764e57831641ab675e448f8
Justin Gray <jgray82@protonmail.com>
2026-04-24 09:21:08 -0400

    add removal to user permission function

--- public/Users_v2/Set-Five9CloudUserPermission.ps1
+++ public/Users_v2/Set-Five9CloudUserPermission.ps1
@@ -1,6 +1,11 @@
 ﻿function Set-Five9CloudUserPermission {
-    param([string]$UserUID, [string]$Username, [string]$Permission)
+    param([string]$UserUID, [string]$Username, [string]$Permission, [switch]$Remove)
     if (-not $UserUID) { $UserUID = Resolve-Five9CloudUserUID $Username } ; if (-not $UserUID) { return }
-    $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users/$userUID/permissions/$Permission" -Method Post
-    if ($result -ne $false) { Write-Host "$Permission assigned to $Username" } else { Write-Host "Failed to assign $Permission to $Username"; return $false }
+    if ($Remove) {
+        $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users/$userUID/permissions/$Permission" -Method Delete
+        if ($result -ne $false) { Write-Host "$Permission removed from $Username" } else { Write-Host "Failed to remove $Permission from $Username"; return $false }
+        } else {
+        $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users/$userUID/permissions/$Permission" -Method Post
+        if ($result -ne $false) { Write-Host "$Permission assigned to $Username" } else { Write-Host "Failed to assign $Permission to $Username"; return $false }
+    }
 }
