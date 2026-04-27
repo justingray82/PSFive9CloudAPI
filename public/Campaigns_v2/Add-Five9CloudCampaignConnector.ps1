06019d46d3926fdc9d0f9734ee0985fe05eb2b3a
Justin Gray <justingray82@gmail.com>
2026-04-27 10:46:17 -0400

    Adding ability to add connector to campaign

---
+++ public/Campaigns_v2/Add-Five9CloudCampaignConnector.ps1
@@ -0,0 +1,7 @@
+﻿function Add-Five9CloudCampaignConnector {
+    param([string]$CampaignId, [string]$CampaignName, [string]$ConnectorName, [string]$ConnectorId)
+    if (-not $campaignId) { $campaignId = Resolve-Five9CloudCampaignId $CampaignId $CampaignName } ; if (-not $campaignId) { return }
+    if (-not $connectorId) { $connectorId = Resolve-Five9CloudConnectorId $ConnectorName } ; if (-not $connectorId) { return }
+    $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/campaigns/v1/domains/$($global:Five9.DomainId)/campaigns/$campaignId/connectors/$connectorId" -Method Post
+    if ($result -ne $false) { Write-Host "$ConnectorName added to $CampaignName"} else { Write-Host "Failed to add $ConnectorName to $CampaignName"; return }
+}
