2230dbfbb14e6b6cf104b496041e8932c4f0c31e
Justin Gray <justingray82@gmail.com>
2026-05-04 14:33:13 -0400

    Added permissionset helper to Add Set to User

--- public/Permissions_v2/Add-Five9CloudPermissionSetToUser.ps1
+++ public/Permissions_v2/Add-Five9CloudPermissionSetToUser.ps1
@@ -1,6 +1,7 @@
 ﻿function Add-Five9CloudPermissionSetToUser {
-    param([Parameter(Mandatory = $true)][string]$UserUID,[Parameter(Mandatory = $true)][string]$PermissionSet)
-    $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users/$UserUID/roles/$PermissionSet" -Method Post
+    param([Parameter(Mandatory = $true)][string]$Username,[string]$UserUID,[Parameter(Mandatory = $true)][string]$PermissionSet,[string]$SetID)
+    if (-not $UserUID) { $UserUID = Resolve-Five9CloudUserUID $Username } ; if (-not $UserUID) { return }
+    if (-not $SetID) { $SetID = Resolve-Five9CloudPermissionSetID $PermissionSet } ; if (-not $SetID) { return }
+    $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users/$UserUID/roles/$SetID" -Method Post
     if ($result -ne $false) { Write-Host "User $UserUID added to permission $PermissionSet" } else { Write-Host "Failed to add $UserUID to permission $PermissionSet" }
 }
-
