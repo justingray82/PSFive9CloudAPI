f71f91e06657d3ea5ee322c0c58884fd80d0f19d
Justin Gray <justingray82@gmail.com>
2026-05-14 13:32:35 -0400

    added list option for acldetails if username and useruid is ommited.

--- public/Users_v2/Get-Five9CloudUserACLDetails.ps1
+++ public/Users_v2/Get-Five9CloudUserACLDetails.ps1
@@ -1,6 +1,12 @@
 ﻿function Get-Five9CloudUserACLDetails {
     param([string]$UserUID, [string]$Username, [ValidateSet("permissions","roles","applicationSets","applications")][Parameter(Mandatory)][string]$Scope)
-    $UserUID = Resolve-Five9CloudUserUID $Username; if (-not $UserUID) { return }
-    $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users?userUID=$($UserUID)"
-    $result.items.$Scope
+    if (-not $UserUID -and $Username) { $UserUID = Resolve-Five9CloudUserUID $Username; if (-not $UserUID) { return } }
+    if ($UserUID) {
+        $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users?userUID=$UserUID"
+    } else {
+        $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users"
+    }
+
+    if (-not $result) { return }
+    if ($UserUID) { $result.items.$Scope } else { $result.items }
 }
