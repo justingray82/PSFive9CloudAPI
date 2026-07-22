891abb316baa416fc712c6f509be3a876b79db4b
Justin Gray <justingray82@gmail.com>
2026-07-22 09:20:38 -0400

    added circles to get acl details

--- public/Users_v2/Get-Five9CloudUserACLDetails.ps1
+++ public/Users_v2/Get-Five9CloudUserACLDetails.ps1
@@ -1,12 +1,20 @@
 ﻿function Get-Five9CloudUserACLDetails {
-    param([string]$UserUID, [string]$Username, [ValidateSet("permissions","roles","applicationSets","applications")][Parameter(Mandatory)][string]$Scope)
+    param([string]$UserUID, [string]$Username, [ValidateSet("permissions","roles","applicationSets","applications","circles")][Parameter(Mandatory)][string]$Scope)
     if (-not $UserUID -and $Username) { $UserUID = Resolve-Five9CloudUserUID $Username; if (-not $UserUID) { return } }
-    if ($UserUID) {
-        $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users?userUID=$UserUID"
+    if($Scope -eq 'circles') {
+        if ($UserUID) {
+            $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/circles/v1/domains/$($global:Five9.DomainId)/circles/users/$UserUID"
+            $result.items.name
+        } else {
+            Write-Host "Username must be supplied for Circles scope." -ForegroundColor Red; return $null
+        }
     } else {
-        $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users"
+        if ($UserUID) {
+            $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users?userUID=$UserUID"
+        } else {
+            $result = Invoke-Five9CloudApi "$($global:Five9.ApiBaseUrl)/acl/v1/domains/$($global:Five9.DomainId)/users"
+        }
+        if (-not $result) { return }
+        if ($UserUID) { $result.items.$Scope } else { $result.items }
     }
-
-    if (-not $result) { return }
-    if ($UserUID) { $result.items.$Scope } else { $result.items }
 }
