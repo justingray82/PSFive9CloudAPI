8da9951f107fc8ae10ce7495dda7dc35b6800a72
Justin Gray <justingray82@gmail.com>
2026-08-04 16:17:03 -0400

    correcting ani group helper function

--- public/Helpers_v2/Resolve-Five9CloudAniGroupId.ps1
+++ public/Helpers_v2/Resolve-Five9CloudAniGroupId.ps1
@@ -1,4 +1,4 @@
-﻿function Resolve-Five9CloudAniId ([string]$AniGroupName) {
+﻿function Resolve-Five9CloudAniGroupId ([string]$AniGroupName) {
     $result = Get-Five9CloudAniGroups
     $filteredResult = $result | ? { $_.name -eq "$($AniGroupName)" }
     if ($filteredResult.name -ne $null) { return $filteredResult.aniGroup.aniGroupId }
